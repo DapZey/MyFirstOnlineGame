a1132e779d9478ea540c2603cd67bbf173b58bee
dapzey <benradoff@gmail.com>
2024-06-24 22:46:45 +0300

    exact coords

--- Client/main.cpp
+++ Client/main.cpp
@@ -32,72 +32,72 @@ int main(int argc, char* argv[]) {
     int averageRTT = 0;
     long long totalRTT = 0;
     while (!WindowShouldClose()) {
-         if (foundConnection ==0){
+        if (foundConnection ==0){
             foundConnection = connectionPage.ShowPage();
-         }
-         else {
-             gameWindow.runGame();
-             auto now = std::chrono::steady_clock::now();
-             auto elapsedGeneral = std::chrono::duration_cast<std::chrono::milliseconds>(now - lastSendRecvTimeGeneral);
-             auto elapsedRTT = std::chrono::duration_cast<std::chrono::milliseconds>(now - lastSendRecvTimeRTT);
-             if (elapsedGeneral.count() >= updateFreq + 1) {
-                 lastSendRecvTimeGeneral = now;
-                 network.sendCoordinates(gameWindow.direction.x,gameWindow.direction.y);
-             }
-             if (elapsedRTT.count() >= 10000) {
-                 lastSendRecvTimeRTT = now;
-                 RTTGap = now;
-                 network.sendRTT();
-             }
-             std::string coords = network.receiveData();
-             std::vector<std::string> player2coords = Utils::splitstringbychar(coords, ",");
-             if (coords[0] == '%'){
-                 auto time = std::chrono::duration_cast<std::chrono::milliseconds>(now - RTTGap);
-                 currentRTT = time.count();
-                 rttCount++;
-                 totalRTT += currentRTT;
-                 averageRTT = (int)(totalRTT / rttCount);
-                 updateFreq = (int)(0.9 * currentRTT + 0.1 * averageRTT);
-                 std::cout << updateFreq << "\n";
-             }
-             else if(coords[0] == '&'){
-                 network.receiveRTT();
-                 if (player2coords.size() > 1) {
-                     gameWindow.x = std::stoi(Utils::splitstringbychar(player2coords[0], "&")[1]);
-                     gameWindow.y = std::stoi(player2coords[1]);
-                 }
-             }
-             else if (player2coords.size() > 1){
-                 gameWindow.newPlayerx = std::stoi(player2coords[0]);
-                 gameWindow.newPlayerY = std::stoi(player2coords[1]);
-             }
-         }
-         if (foundConnection == 1){
-             std::vector<std::string> data = Utils::splitstringbychar(connectionPage.name,"/");
-             int connectionTry = 1;
-             if (data.size() != 3){
-                 foundConnection = 0;
-                 continue;
-             }
-             try {
-                 connectionTry = network.init(data[0], std::stoi(data[1]));
-             } catch (const std::exception& e) {
-                 foundConnection = 0;
-                 std::cerr << "Error during connection initialization: " << e.what() << std::endl;
-             }
-             if (connectionTry == 0){
-                 network.createSocket();
-                 gameWindow.setUsername(data[2]);
-                 //TODO: send server hello
-                 std::cout<<"socket created";
-                 foundConnection = 2;
-             }
-             else {
-                 foundConnection = 0;
-             }
-             //initialize connection to server
-             //proceed to show game screen
-         }
+        }
+        else {
+            gameWindow.runGame();
+            auto now = std::chrono::steady_clock::now();
+            auto elapsedGeneral = std::chrono::duration_cast<std::chrono::milliseconds>(now - lastSendRecvTimeGeneral);
+            auto elapsedRTT = std::chrono::duration_cast<std::chrono::milliseconds>(now - lastSendRecvTimeRTT);
+            if (elapsedGeneral.count() >= updateFreq + 1) {
+                lastSendRecvTimeGeneral = now;
+                network.sendCoordinates(gameWindow.x,gameWindow.y);
+            }
+            if (elapsedRTT.count() >= 10000) {
+                lastSendRecvTimeRTT = now;
+                RTTGap = now;
+                network.sendRTT();
+            }
+            std::string coords = network.receiveData();
+            std::vector<std::string> player2coords = Utils::splitstringbychar(coords, ",");
+            if (coords[0] == '%'){
+                auto time = std::chrono::duration_cast<std::chrono::milliseconds>(now - RTTGap);
+                currentRTT = time.count();
+                rttCount++;
+                totalRTT += currentRTT;
+                averageRTT = (int)(totalRTT / rttCount);
+                updateFreq = (int)(0.9 * currentRTT + 0.1 * averageRTT);
+                std::cout << updateFreq << "\n";
+            }
+            else if(coords[0] == '&'){
+                network.receiveRTT();
+                if (player2coords.size() > 1) {
+                    gameWindow.x = std::stoi(Utils::splitstringbychar(player2coords[0], "&")[1]);
+                    gameWindow.y = std::stoi(player2coords[1]);
+                }
+            }
+            else if (player2coords.size() > 1){
+                gameWindow.newPlayerx = std::stoi(player2coords[0]);
+                gameWindow.newPlayerY = std::stoi(player2coords[1]);
+            }
+        }
+        if (foundConnection == 1){
+            std::vector<std::string> data = Utils::splitstringbychar(connectionPage.name,"/");
+            int connectionTry = 1;
+            if (data.size() != 3){
+                foundConnection = 0;
+                continue;
+            }
+            try {
+                connectionTry = network.init(data[0], std::stoi(data[1]));
+            } catch (const std::exception& e) {
+                foundConnection = 0;
+                std::cerr << "Error during connection initialization: " << e.what() << std::endl;
+            }
+            if (connectionTry == 0){
+                network.createSocket();
+                gameWindow.setUsername(data[2]);
+                //TODO: send server hello
+                std::cout<<"socket created";
+                foundConnection = 2;
+            }
+            else {
+                foundConnection = 0;
+            }
+            //initialize connection to server
+            //proceed to show game screen
+        }
 
     }
 
